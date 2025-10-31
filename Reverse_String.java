7bdbef8b8f70acae817556eb83049f7baaef000b
KartikayAwsthi <ikartikayAwasthii@gmail.com>
2025-10-31 11:08:26 +0530

    reverse_String added

---
+++ Reverse_String.java
@@ -0,0 +1,14 @@
+public class Reverse_String
+{
+    public static void main(String[] args) {
+        String str="Kartikay";
+        int n=str.length();
+        String rev="";
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            rev = rev + str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+
+}
